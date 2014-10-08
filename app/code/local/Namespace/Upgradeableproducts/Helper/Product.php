cabecff6c6b9fb59ccf39c935ee4a3ccad184807
Aziz Pulatov <aziz.pulatov@gmail.com>
2014-10-08 13:50:00 -0500

    Update Product.php

--- app/code/local/Namespace/Upgradeableproducts/Helper/Product.php
+++ app/code/local/Namespace/Upgradeableproducts/Helper/Product.php
@@ -4,8 +4,8 @@
  * Product helper extensions.
  *
  * @package   Namespace_Upgradeableproducts
- * @author    Name <name@email.com>
- * @copyright 2014 Your Company
+ * @author    Aziz Pulatov <aziz.pulatov@gmail.com>
+ * @copyright 2014 WaveMachine Labs Inc.
  */
 
 class Namespace_Upgradeableproducts_Helper_Product
